c655b0630494c74b4b1541c287bba13a3d261fcc
notauserx <82922717+notauserx@users.noreply.github.com>
2023-05-27 01:19:50 +0600

    add tests

--- tests/createSeat.test.ts
+++ tests/createSeat.test.ts
@@ -1,8 +1,16 @@
 import { createSeat } from "../src/Factories";
-import { LeftSeatingSection, SeatPosition, WindowSeat } from "../src/Types";
+import { AisleSeat, LeftSeatingSection, MiddleSeat, SeatPosition, WindowSeat } from "../src/Types";
 
 describe('testing create seat', () => {
-    test('first col of left seating section seat should be windowSeat', () => {
+    test('first col of left seating section seat should be a window Seat', () => {
       expect(createSeat(new SeatPosition(1, 0, new LeftSeatingSection(2, 2)))).toBeInstanceOf(WindowSeat);
+    });
+
+    test('last col of left seating section seat should be a aisle seat', () => {
+      expect(createSeat(new SeatPosition(1, 1, new LeftSeatingSection(2, 2)))).toBeInstanceOf(AisleSeat);
+    });
+
+    test('middle col of left seating section seat should be windowSeat', () => {
+      expect(createSeat(new SeatPosition(1, 1, new LeftSeatingSection(2, 3)))).toBeInstanceOf(MiddleSeat);
     });
   });
