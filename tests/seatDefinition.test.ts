4c51cba86461cb5c678ffab1f0b967778b0c6b93
notauserx <82922717+notauserx@users.noreply.github.com>
2023-05-27 20:51:58 +0600

    format file

--- tests/seatDefinition.test.ts
+++ tests/seatDefinition.test.ts
@@ -1,29 +1,56 @@
 import { SeatDefinition } from "../src/Domain/SeatDefinition";
 import { SeatPosition } from "../src/Seat";
-import { LeftSeatingSection, RightSeatingSection, SingleSeatingSection } from "../src/SeatingSection";
+import { LeftSeatingSection, MiddleSeatingSection, RightSeatingSection, SingleSeatingSection } from "../src/SeatingSection";
 
-describe('testing seat definition', () => {
-    test('first col of left seating section is a window seat', () => {
-      expect(
-        SeatDefinition.isLeftWindowSeat(new SeatPosition(0, 0, new LeftSeatingSection(2,2))))
-        .toBe(true);
+describe('testing seat definitions', () => {
+    describe('testing aisle seat definitions', () => {
+        test('last col of left seating section is an aisle seat', () => {
+            expect(
+                SeatDefinition.isAisleSeat(new SeatPosition(0, 1, new LeftSeatingSection(2, 2))))
+                .toBe(true);
+        });
+
+        test('first col of right seating section is an aisle seat', () => {
+            expect(
+                SeatDefinition.isAisleSeat(new SeatPosition(0, 0, new RightSeatingSection(2, 2))))
+                .toBe(true);
+        });
+
+        test('first col of a middle seating section is an aisle seat', () => {
+            expect(
+                SeatDefinition.isAisleSeat(new SeatPosition(0, 0, new MiddleSeatingSection(2, 2))))
+                .toBe(true);
+        });
+
+        test('last col of a middle seating section is an aisle seat', () => {
+            expect(
+                SeatDefinition.isAisleSeat(new SeatPosition(0, 0, new MiddleSeatingSection(2, 2))))
+                .toBe(true);
+        });
     });
+    describe('testing window seat definitions', () => {
+        test('first col of left seating section is a window seat', () => {
+            expect(
+                SeatDefinition.isLeftWindowSeat(new SeatPosition(0, 0, new LeftSeatingSection(2, 2))))
+                .toBe(true);
+        });
 
-    test('last col of right seating section is a window seat', () => {
-        expect(
-            SeatDefinition.isRightWindowSeat(new SeatPosition(0, 1, new RightSeatingSection(2,2))))
-            .toBe(true);
+        test('last col of right seating section is a window seat', () => {
+            expect(
+                SeatDefinition.isRightWindowSeat(new SeatPosition(0, 1, new RightSeatingSection(2, 2))))
+                .toBe(true);
+        });
+
+        test('first col of single seating section is a window seat', () => {
+            expect(
+                SeatDefinition.isLeftWindowSeat(new SeatPosition(0, 0, new SingleSeatingSection(2, 2))))
+                .toBe(true);
+        });
+
+        test('last col of single seating section is a window seat', () => {
+            expect(
+                SeatDefinition.isRightWindowSeat(new SeatPosition(0, 1, new SingleSeatingSection(2, 2))))
+                .toBe(true);
+        });
     });
-
-    test('first col of single seating section is a window seat', () => {
-        expect(
-          SeatDefinition.isLeftWindowSeat(new SeatPosition(0, 0, new SingleSeatingSection(2,2))))
-          .toBe(true);
-      });
-  
-      test('last col of single seating section is a window seat', () => {
-          expect(
-              SeatDefinition.isRightWindowSeat(new SeatPosition(0, 1, new SingleSeatingSection(2,2))))
-              .toBe(true);
-      });
-  });
+});
